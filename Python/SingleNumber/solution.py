b4c82c8b5692aa1038635b4732590353ced4f745
Yang Song <ysong819@gmail.com>
2014-11-09 18:34:28 -0800

    SingleNumber Python Solved

--- Python/SingleNumber/solution.py
+++ Python/SingleNumber/solution.py
@@ -7,9 +7,10 @@ class Solution:
 # @parm A, a list of integer
 # @return an interger
     def singleNumber(self, A):
-        temp = set(A)
-        temp = list(temp)
-        print temp
+        s = A[0]
+        for a in range(1, len(A)):
+            s = s ^ A[a]
+        return s
 
     def singleNumberFail(self, A):
         temp = []
@@ -22,8 +23,10 @@ class Solution:
 
 
 A = [1,2,3,4,5,6,3,4,6,5,8,1,2]
-print A
+B = [-1]
+#print A
 test = Solution()
+#print test.singleNumber(A)
+#print test.singleNumberFail(A)
+#print A
 print test.singleNumber(A)
-print test.singleNumberFail(A)
-print A
